ca5fdbe2e3f25f6ebcea4efd358c71187a453ccb
lekret <lekret399@mail.ru>
2023-10-18 20:22:30 +0300

    Inline query fix

--- Assets/Scripts/Systems/RemoveAllSystem.cs
+++ Assets/Scripts/Systems/RemoveAllSystem.cs
@@ -8,6 +8,7 @@ namespace Systems
     public class RemoveAllSystem<T> : BaseSystem
     {
         private readonly QueryDescription _query = new QueryDescription().WithAll<T>();
+        private InlineQuery _inlineQuery;
         
         public RemoveAllSystem(World world) : base(world)
         {
@@ -15,7 +16,15 @@ namespace Systems
 
         public override void Update(in float deltaTime)
         {
-            World.Query(_query, entity => entity.Remove<T>());
+            World.InlineEntityQuery<InlineQuery, T>(_query, ref _inlineQuery);
+        }
+
+        private struct InlineQuery : IForEachWithEntity<T>
+        {
+            public void Update(Entity entity, ref T t0)
+            {
+                entity.Remove<T>();
+            }
         }
     }
 }
