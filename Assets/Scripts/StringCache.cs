25391b6101c707a53b611a8535e67847093680a7
lekret <lekret399@mail.ru>
2023-12-22 23:29:45 +0300

    Fixed string cache

--- Assets/Scripts/StringCache.cs
+++ Assets/Scripts/StringCache.cs
@@ -2,14 +2,14 @@
 
 public static class StringCache<T>
 {
-    private static readonly Dictionary<T, string> _cache = new();
+    private static readonly Dictionary<(string Format, T Value), string> _cache = new();
 
     public static string Get(string format, T value)
     {
-        if (!_cache.TryGetValue(value, out var cachedString))
+        if (!_cache.TryGetValue((format, value), out var cachedString))
         {
             cachedString = string.Format(format, value);
-            _cache.Add(value, cachedString);
+            _cache.Add((format, value), cachedString);
         }
 
         return cachedString;
